5704b0567109142f532b2a14bbf004262321d139
MaevaG <maeva.garcia13210@gmail.com>
2024-06-06 09:57:27 +0200

    DEBUT LOGIN

---
+++ FrontEnd/login.js
@@ -0,0 +1,21 @@
+
+//SELECTION DU BUTTON**
+
+const loginbtn = document.querySelector(".login");
+
+
+//loginbtn.addEventListener("click", () => //...) // ouverture page connexion)
+
+
+
+// Création de la page**
+
+// Création de la page**
+    const OuvertureForm = () => {
+    const pageForm = document.querySelector("main");
+    pageForm.innerHTML = ''; 
+};
+
+
+
+
