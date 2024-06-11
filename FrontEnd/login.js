e30f7914c4ea67f48e705169496c75d262c5c050
MaevaG <maeva.garcia13210@gmail.com>
2024-06-11 17:23:14 +0200

    AJOUT LOGIN

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,6 +1,6 @@
 function ajoutListenerLogin() {
     const loginform = document.querySelector(".loginform");
-    loginform.addEventListener("submit", function(event) {
+    loginform.addEventListener("submit", async function(event) {
         event.preventDefault();
 
         const balisemail = document.getElementById("loginemail");
@@ -13,22 +13,38 @@ function ajoutListenerLogin() {
 
         const errorMessage = document.getElementById("errorMessage");
 
-        // Vérification des informations de connexion
-        if (loginmail === "sophie.bluel@test.tld" && loginpassword === "S0phie") {
-            // Redirection vers la page d'accueil
-            window.location.href = "index.html";
+        try {
+            const response = await fetch('http://localhost:5678/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email: loginmail, password: loginpassword })
+            });
 
-            // Stockage du token d'authentification 
-            localStorage.setItem("authToken", "your-auth-token-here");
-        } else {
-            // Affichage d'un message d'erreur
-            errorMessage.textContent = "Mot de passe ou email incorrect";
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                // Redirection vers la page index et stockage du token
+                window.location.href = 'index.html';
+                localStorage.setItem('token', data.token);
+            } else {
+                // Affichage du message d'erreur en cas de problème avec la connexion
+                errorMessage.textContent = 'Adresse email ou mot de passe incorrect.';
+            }
+        } catch (error) {
+            console.error('Erreur lors de la tentative de connexion :', error);
+            errorMessage.textContent = 'Une erreur s\'est produite. Veuillez réessayer.';
         }
     });
 }
 
-
 ajoutListenerLogin();
+
+        // CREER PAGE ADMINISTRATION EN DEMANDANT SI LE TOKEN EST BIEN BON
+        // MODIFIER HTML VIA DOM
+
+
 
 
 
