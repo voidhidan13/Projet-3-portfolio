51b0a5669b44b4d94d50a4fdb5e377e29571265a
MaevaG <maeva.garcia13210@gmail.com>
2024-06-13 17:05:41 +0200

    Ajout mode edition

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -2,11 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     // Vérifier si le token est présent dans le stockage local
     const token = localStorage.getItem('token');
 
-    // Sélectionnez l'élément <li> contenant le lien de connexion
+    // Sélectionner l'élément <li> contenant le lien de connexion
     const loginText = document.querySelector('.login');
 
     // Si le token est présent, mettez à jour le texte du lien
     if (token) {
         loginText.innerHTML = '<a href="#">logout</a>';
+
+        // Créer une nouvelle div pour le message de bienvenue avec une icône FontAwesome
+        const welcomeMessageDiv = document.createElement('div');
+        welcomeMessageDiv.id = 'welcome-message';
+        welcomeMessageDiv.innerHTML = '<p><i class="fas fa-pen-to-square"></i> Mode Edition</p>';
+
+        // Insérer la div de bienvenue avant le header
+        const body = document.body;
+        body.insertBefore(welcomeMessageDiv, body.firstChild);
     }
 });
